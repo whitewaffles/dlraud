b5657a5e01b337ed65e637b97d452d7ba11b9913
whitewaffles <113029001+whitewaffles@users.noreply.github.com>
2024-12-01 21:13:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 async function fetchPosts() {
-    const response = await fetch('https://synonymous-detailed-buffer/getPosts');  // Glitch 서버 URL로 수정
+    const response = await fetch('https://synonymous-detailed-buffer.glitch.me/getPosts');  // Glitch 서버 URL로 수정
     const posts = await response.json();
     const postsList = document.getElementById('posts-list');
 
